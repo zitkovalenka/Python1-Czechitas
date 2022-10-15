75cfca32d4417b5b5f9422a0681aabe4ae19cada
zitkovalenka <114017674+zitkovalenka@users.noreply.github.com>
2022-10-15 16:00:43 +0200

    Update ukol-04_Tridy-Kucharka.py

--- ukol-04_Tridy-Kucharka.py
+++ ukol-04_Tridy-Kucharka.py
@@ -17,6 +17,34 @@ class Recept:
     def zkusit(self):
         self.vyzkouseno = True
 
+class Kucharka:
+    def __init__(self, nazev, autor, recepty=[]):
+        self.nazev = nazev
+        self.autor = autor
+        self.recepty = recepty
+    def __str__(self):
+        return f"{self.nazev} od {self.autor} ({self.pocet_receptu})"
+    def pocet_receptu(self):
+        self.pocet_receptu = 0
+        for i in self.recepty: #self.pocet_receptu:  Kucharka:
+            self.pocet_receptu = self.pocet_receptu + 1    # for cyklus?
+        return self.pocet_receptu
+    def pridej_recept(self, Recept):
+        self.recept = Recept
+ 
+recept_1 = Recept("Štrůdl", 3, "https://www.nejrecept.cz/recept/fantasticky-stavnaty-jablecny-zavin-r5687")
+recept_1.zmen_narocnost(5)
+print(recept_1)
+recept_2 = Recept("Bábovka", 2, "https://www.toprecepty.cz/recept/1673-hrnickova-babovka/")
+recept_2.zkusit()
+print(recept_2)   
+
+
+kucharka=Kucharka("Dobrůdky", "Lentilka")
+print(kucharka) 
+kucharka.pridej_recept(recept_1)
+print(kucharka.pocet_receptu)
+
 
 """class Kucharka:
     def __init__(self, k_nazev, strana):
@@ -28,25 +56,30 @@ class Recept:
         self.pocet = 0
         self.pocet = self.pocet + 1"""
 
-class Kucharka:
+"""class Kucharka:
     def __init__(self, nazev_kucharky, pocet_receptu=0):
         self.nazev_kucharky = nazev_kucharky
         self.pocet_receptu = pocet_receptu
     def __str__(self):
         return f"{self.nazev_kucharky}: {self.pocet_receptu} receptů."
     def pocet_receptu(self):
-        self.pocet = 0
-        self.pocet = self.pocet + 1    # for cyklus?
+        self.pocet_receptu = 0
+        for i in Kucharka: #self.pocet_receptu:
+            self.pocet_receptu = self.pocet_receptu + 1    # for cyklus?
+        return self.pocet_receptu
     def pridej_recept(self, Recept):
-        self.recept = Recept()
+        self.recept = Recept
+"""
+
+"""def get_sleva(self, procento):
+        self.cena = self.cena*(1-procento/100)
+
+kniha1 = Kniha("Pokemon",495,279)
+kniha1.get_sleva(20)
+kniha2 = Kniha("Lolita",252,249)"""
 
 
-recept_1 = Recept("Štrůdl", 3, "https://www.nejrecept.cz/recept/fantasticky-stavnaty-jablecny-zavin-r5687")
-recept_1.zmen_narocnost(5)
-print(recept_1)
-recept_2 = Recept("Bábovka", 2, "https://www.toprecepty.cz/recept/1673-hrnickova-babovka/")
-recept_2.zkusit()
-print(recept_2)
+
 
 """
 kucharka_1 = Kucharka("Štrůdl",5)
@@ -54,10 +87,6 @@ kucharka_2 = Kucharka("Bábovka",7)
 print(kucharka_1)
 print(kucharka_2)
 """
-kucharka=Kucharka("Dobrůdky od Lentilky")
-print(kucharka) 
-kucharka.pridej_recept(recept_1)
-print(kucharka.pocet_receptu)
 
 
 
