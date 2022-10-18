a71419f655b2abb0b12bb165c20634c39664a362
zitkovalenka <114017674+zitkovalenka@users.noreply.github.com>
2022-10-18 13:25:24 +0200

    Update ukol-04_Tridy-Kucharka.py

--- ukol-04_Tridy-Kucharka.py
+++ ukol-04_Tridy-Kucharka.py
@@ -18,19 +18,24 @@ class Recept:
         self.vyzkouseno = True
 
 class Kucharka:
-    def __init__(self, nazev, autor, recepty=[]):
+    def __init__(self, nazev, autor):
         self.nazev = nazev
         self.autor = autor
-        self.recepty = recepty
+        self.recepty = []
     def __str__(self):
-        return f"{self.nazev} od {self.autor} ({self.pocet_receptu})"
+        return f"V kuchařce {self.nazev} od {self.autor} je {self.pocet_receptu()} receptů." 
     def pocet_receptu(self):
-        self.pocet_receptu = 0
-        for i in self.recepty: #self.pocet_receptu:  Kucharka:
-            self.pocet_receptu = self.pocet_receptu + 1    # for cyklus?
-        return self.pocet_receptu
-    def pridej_recept(self, Recept):
-        self.recept = Recept
+        self.pocet_receptu_prom = 0
+        for i in self.recepty: 
+            self.pocet_receptu_prom = self.pocet_receptu_prom + 1
+        return self.pocet_receptu_prom
+    """def pocet_receptu(self):
+        pocet_receptu = len(self.recepty)
+        #print(f"V kucharce je {pocet_receptu} receptů.")  
+        return pocet_receptu"""
+    def pridej_recept(self, novy_recept):
+        self.recepty.append(novy_recept)
+
  
 recept_1 = Recept("Štrůdl", 3, "https://www.nejrecept.cz/recept/fantasticky-stavnaty-jablecny-zavin-r5687")
 recept_1.zmen_narocnost(5)
@@ -41,86 +46,15 @@ print(recept_2)
 
 
 kucharka=Kucharka("Dobrůdky", "Lentilka")
-print(kucharka) 
-kucharka.pridej_recept(recept_1)
-print(kucharka.pocet_receptu)
+print(kucharka)
+kucharka.pocet_receptu()            # musím nejprve fci zavolat, abych mohla vypsat výsledek
+#print(kucharka.pocet_receptu)
+kucharka.pridej_recept(recept_1)    # musím nejprve fci zavolat, abych mohla vypsat výsledek
+kucharka.pocet_receptu()            # musím nejprve fci zavolat, abych mohla vypsat výsledek
+# print(kucharka.pocet_receptu())
+print(kucharka)
+kucharka.pridej_recept(recept_2) 
+kucharka.pocet_receptu()     
+print(kucharka)
 
-
-"""class Kucharka:
-    def __init__(self, k_nazev, strana):
-        self.k_nazev = k_nazev
-        self.strana = strana
-    def __str__(self):
-        return f"{self.k_nazev}                 str {self.strana}"
-    def pocet_receptu(self):
-        self.pocet = 0
-        self.pocet = self.pocet + 1"""
-
-"""class Kucharka:
-    def __init__(self, nazev_kucharky, pocet_receptu=0):
-        self.nazev_kucharky = nazev_kucharky
-        self.pocet_receptu = pocet_receptu
-    def __str__(self):
-        return f"{self.nazev_kucharky}: {self.pocet_receptu} receptů."
-    def pocet_receptu(self):
-        self.pocet_receptu = 0
-        for i in Kucharka: #self.pocet_receptu:
-            self.pocet_receptu = self.pocet_receptu + 1    # for cyklus?
-        return self.pocet_receptu
-    def pridej_recept(self, Recept):
-        self.recept = Recept
-"""
-
-"""def get_sleva(self, procento):
-        self.cena = self.cena*(1-procento/100)
-
-kniha1 = Kniha("Pokemon",495,279)
-kniha1.get_sleva(20)
-kniha2 = Kniha("Lolita",252,249)"""
-
-
-
-
-"""
-kucharka_1 = Kucharka("Štrůdl",5)
-kucharka_2 = Kucharka("Bábovka",7) 
-print(kucharka_1)
-print(kucharka_2)
-"""
-
-
-
-
-#-------------------------------------------------------------------------------------------------------------
-"""class Recept:
-    def __init__(self, nazev, narocnost, url_adresa,vyzkouseno=False):
-        self.nazev = nazev
-        self.narocnost = narocnost
-        self.url_adresa = url_adresa
-        self.vyzkouseno = vyzkouseno
-
-    def __str__(self):
-        if self.vyzkouseno == True:
-            return f"Recept {self.nazev} (náročnost {self.narocnost}) vyzkoušeno: {self.url_adresa}"
-        else:
-            return f"Recept {self.nazev} (náročnost {self.narocnost}) nevyzkoušeno: {self.url_adresa}"
-
-    def zmen_narocnost(self, nova_narocnost):
-        self.narocnost = nova_narocnost
-
-    def zkusit(self):
-        self.vyzkouseno = True
-
-
-class Kucharka
-
-
-
-recept_1 = Recept("Štrůdl", 3, "https://www.nejrecept.cz/recept/fantasticky-stavnaty-jablecny-zavin-r5687")
-recept_1.zmen_narocnost(5)
-print(recept_1)
-recept_2 = Recept("Bábovka", 2, "https://www.toprecepty.cz/recept/1673-hrnickova-babovka/")
-recept_2.zkusit()
-print(recept_2)
-"""
 
