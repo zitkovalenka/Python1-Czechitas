99b191591e96a8399cefccdff10d2ac213fe705f
Lenka Zítková <zitkovalenka@gmail.com>
2022-11-05 19:10:35 +0100

    Odevzdávám úkol bez bonusu

--- ukol-06-Zapis_a_cteni_ze_souboru-Znamky.py
+++ ukol-06-Zapis_a_cteni_ze_souboru-Znamky.py
@@ -20,26 +20,20 @@ with open("ukol-06-znamky_za_semestr-opraveno.txt","a", encoding="utf-8") as vys
 # zapisu radky[0] do souboru / ulozi msi do modifikovanych radku
 # pro kazdy radek v radky[1:] zapisu radek s replacnutymi hodnotami / ulozi msi h odo seznamu k zapsani
 
-"""with open("ukol-06-znamky_za_semestr.txt","r", encoding="utf-8") as vstup:
-    # prvni_radek = vstup.readline()
-    radky = vstup.readlines()
+# dle koučky
 
-radek = [radek.split() for radek in radky]
-# print(radek)
+with open("ukol-06-znamky_za_semestr.txt","r", encoding="utf-8") as vstup:
+    radky = vstup.readlines()     # readlines() vytvoří seznam stringů a rozděluje soubor na řádky pomocí \n. Není tedy potřeba radek = [radek.split() for radek in radky]   
 
-if radek[0]:
-    prvni_radek = radky[0]
-    print(prvni_radek)
-for radek in radek[1:]:
-    znamky_replace = [radek.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5","F") for radek in radky]
-    print(znamky_replace)
+radek_1 = radky[0]
+znamky_replace = [radek.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5","F") for radek in radky[1:]]
+
+k_zapsani = [radek_1] + znamky_replace
+
 with open("ukol-06-znamky_za_semestr-opraveno.txt","w", encoding="utf-8") as vystup:
-    if radek[0]:
-        prvni_radek = radky[0]
-        # print(prvni_radek)
-    for radek in radek[1:]:
-        znamky_replace = [radek.replace("1","A").replace("2","B").replace("3","C").replace("4","D").replace("5","F") for radek in radky]
-        # print(znamky_replace)
-    
-    # vystup.writelines(znamky_replace)
-"""
+    vystup.writelines(k_zapsani)   # write zapíše jen jeden řádek, writelines() píše víc řádku v listu najednou.
+
+    # a nebo
+    # vystup.write(radek_1)
+    # for radek in znamky_replace:
+    #     vystup.write(radek)
